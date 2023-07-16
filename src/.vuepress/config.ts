aa239e887d4482fa6f30441f6f0df69483dd8fc4
G. Willson <greg@biomassiv.es>
2023-07-16 14:45:24 -0600

    Update config.ts

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -16,6 +16,21 @@ export default defineUserConfig({
       title: "文档演示",
       description: "Sustainable Community Development Hub 可持續社區發展中心",
     },
+    "/hi/": {
+      lang: "hi-IN",
+      title: "हिंदी",
+      description: "सतत सामुदायिक विकास केंद्र",
+    },
+    "/ne/": {
+      lang: ne-NE",
+      title: "नेपाली",
+      description: "दिगो सामुदायिक विकास हब",
+    },
+    "/es/": {
+      lang: es-MX",
+      title: "नेपाली",
+      description: "दिगो सामुदायिक विकास हब",
+    },
   },
   plugins: [
     searchPlugin({
